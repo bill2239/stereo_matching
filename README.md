d41051cf1c87b46fa6023c00e9dfbd70964f2720
lichi huang <li-chi.huang@graftek.com>
2018-10-31 10:44:18 -0500

    add rank and census transform to matching cost

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 stereo matching 
 ==========
 Simple CPU implementation from sratch of Block Matching using simple Sum of Square difference Matching Cost. Also have the option for parallel computing  
-You can either compile the code with gcc or visual studio with Opencv installed   
+You can either compile the code to executable with gcc or visual studio with Opencv installed   
 
 left image:  
 ![image](im1.png)  
 right image:  
 ![image](im2.png)  
-disparity image:  
+disparity image with fixed windows ssd:  
 ![image](depth.png)
+disparity image with rank transform:  
+![image](depth_rank.png)
+disparity image with census transform:  
+![image](depth_census.png)  
+
+Clearly, using rank transform or census transform looks better, probably the nature of these transform make left and right images less sensitive to illumination changes.  
+
 ---
 ## Reqirement 
 
@@ -16,10 +23,10 @@ disparity image:
 
 **Usage**
 
-    ./stereo_mathing -left=<left image> -right=<right image> -max-disparity=<disparity range> -output=<output image file> -parallel=<if you want to run parallel version>
+    ./stereo_mathing -left=<left image> -right=<right image> -max-disparity=<disparity range> -output=<output image file> -parallel=<if you want to run parallel version> -cost=<matching cost function ex: rank or census>
 
 **Examples**
 
-    ./stereo_matching -left=im1.png -right=im2.png -max-disparity=50 -output=depth.png -parallel=no
+    ./stereo_matching -left=im1.png -right=im2.png -max-disparity=50 -output=depth.png -parallel=no -cost=rank
 
 
--- stereo_matching.cpp
+++ stereo_matching.cpp
@@ -8,12 +8,70 @@
 using namespace cv;
 using namespace std;
 
-Mat stereo_match(Mat left,Mat right,int window_size,int max_disparity) {
+Mat rank_transform(Mat image, int windowsize) {
+	int h = image.rows;
+	int w = image.cols;
+	Mat imgDisparity8U = Mat(image.rows, image.cols, CV_8U);
+	int window_half = windowsize / 2;
+
+	for (int y = window_half; y < h - window_half; y++) {
+		for (int x = window_half; x < w - window_half; x++) {
+			int ssd = 0;
+
+			for (int v = -window_half; v < window_half + 1; v++) {
+				for (int u = -window_half; u < window_half + 1; u++) {
+
+					if (image.at<uchar>(y + v, x + u) > image.at<uchar>(y, x)) ssd++;
+				}
+
+			}
+
+			imgDisparity8U.at<uchar>(y, x) = ssd;
+
+		}
+	}
+	return imgDisparity8U;
+}
+Mat census_transform(Mat image, int windowsize) {
+	int h = image.rows;
+	int w = image.cols;
+	Mat imgDisparity8U = Mat(image.rows, image.cols, CV_8U);
+	int window_half = windowsize / 2;
+
+	for (int y = window_half; y < h - window_half; y++) {
+		for (int x = window_half; x < w - window_half; x++) {
+			int ssd = 0;
+
+			for (int v = -window_half; v < window_half + 1; v++) {
+				for (int u = -window_half; u < window_half + 1; u++) {
+					ssd <<= 1;
+					if (image.at<uchar>(y + v, x + u) > image.at<uchar>(y, x)) ssd = ssd | 1;
+					else  ssd = ssd | 0;
+				}
+
+			}
+
+			imgDisparity8U.at<uchar>(y, x) = ssd;
+
+		}
+	}
+	return imgDisparity8U;
+}
+Mat stereo_match(Mat left,Mat right,int window_size,int max_disparity,string cost) {
 	int h = left.rows;
 	int w = left.cols;
 	Mat imgDisparity8U = Mat(left.rows, left.cols, CV_8U);
 	int window_half = window_size / 2;
-	int adjust = 255 / max_disparity; //adjust disparity value to visualize in 8 bit
+	int adjust = 255 / max_disparity;
+	//decide which matching cost function to use 
+	if (cost == "rank") {
+		left = rank_transform(left, 7);
+		right = rank_transform(right, 7);
+	}
+	else if (cost == "census") {
+		left = census_transform(left, 7);
+		right = census_transform(right, 7);
+	}
 	for (int y = window_half; y < h - window_half; y++) {
 		for (int x = window_half; x < w - window_half; x++) {
 			int prev_ssd = INT_MAX;
@@ -21,27 +79,26 @@ Mat stereo_match(Mat left,Mat right,int window_size,int max_disparity) {
 			for (int off = 0; off < max_disparity; off++) {
 				int ssd = 0;
 				int ssd_tmp = 0;
-				// calculate sum of square difference in windows given the offset (disparity)
 				for (int v = -window_half; v < window_half; v++) {
 					for (int u = -window_half; u < window_half; u++) {
 						ssd_tmp = left.at<uchar>(y + v, x + u) - right.at<uchar>(y + v, x + u - off);
 						ssd += ssd_tmp * ssd_tmp;
-					}	
+					}
+					
 				}
-				// store the best disparity (with smallest ssd)
 				if (ssd < prev_ssd) {
-						prev_ssd = ssd;
-						best_dis = off;
+					prev_ssd = ssd;
+					best_dis = off;
 				}
+				
 			}
 			imgDisparity8U.at<uchar>(y, x) = best_dis * adjust;
-		
 		}
-
 	}
 
 	return imgDisparity8U;
 }
+
 
 Mat stereo_match_parallel(Mat left, Mat right, int window_size, int max_disparity) {
 	int h = left.rows;
@@ -91,18 +148,19 @@ Mat stereo_match_parallel(Mat left, Mat right, int window_size, int max_disparit
 int main(int argc, char** argv)
 {
 	cv::CommandLineParser parser(argc, argv,
-		"{left||}{right||}{max-disparity|0|}{output||}{parallel||}");
+		"{left||}{right||}{max-disparity|0|}{output||}{parallel||}{cost||}");
 	string leftname = parser.get<std::string>("left");
 	string rightname = parser.get<std::string>("right");
 	string outname = parser.get<std::string>("output");
 	string parallel = parser.get<std::string>("parallel");
+	string cost = parser.get<std::string>("cost");
 	int max_disparity = parser.get<int>("max-disparity");
 
 	Mat dis;
 	Mat left = imread(leftname,0); //read images into grayscale
 	Mat right = imread(rightname,0);
 	if (parallel == "no"){
-		dis = stereo_match(left, right, 6, max_disparity);
+		dis = stereo_match(left, right, 6, max_disparity, cost);
 	}
 	else dis = stereo_match_parallel(left, right, 6, max_disparity);
 	imwrite(outname,dis);
